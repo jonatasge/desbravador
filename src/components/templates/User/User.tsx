e136aa42f44a545173305fcef4b725804ac99598
Jonatas Evangelista <jonatasge@gmail.com>
2023-09-21 18:18:49 -0300

    fix: Overview responsiveness

--- src/components/templates/User/User.tsx
+++ src/components/templates/User/User.tsx
@@ -4,7 +4,7 @@ import { Overview } from "@/components/orgnanisms";
 import { User } from "@/core/models/User";
 
 export type UserTemplateProps = {
-  user: Partial<User> & {
+  user?: Partial<User> & {
     username?: string;
   };
 };
@@ -14,7 +14,7 @@ export const UserTemplate = ({ user }: UserTemplateProps) => {
     <Container className="UserTemplate p-2">
       <Overview.Root>
         <Overview.Aside
-          avatar={{ src: user?.avatar_url, height: 260, width: 260 }}
+          avatar={{ src: user?.avatar_url }}
           title={{ children: user?.username, className: "my-2" }}
         >
           <Data label="E-mail:">{user?.email || "informação privada"}</Data>
@@ -26,10 +26,10 @@ export const UserTemplate = ({ user }: UserTemplateProps) => {
         </Overview.Aside>
 
         <Overview.Main className="text-left">
-          {user.bio && (
+          {user?.bio && (
             <>
               <Subtitle className="mb-1">Bio:</Subtitle>
-              <Card className="mb-4">{user.bio}</Card>
+              <Card className="mb-4">{user?.bio}</Card>
             </>
           )}
         </Overview.Main>
