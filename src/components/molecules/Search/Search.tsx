afb4518b1315c171ef765a01cbb6477279b02622
Jonatas Evangelista <jonatasge@gmail.com>
2023-09-21 15:51:06 -0300

    feat: changes Search settings

--- src/components/molecules/Search/Search.tsx
+++ src/components/molecules/Search/Search.tsx
@@ -12,7 +12,7 @@ export type SearchProps = Omit<JSX.IntrinsicElements["form"], "onSubmit"> & {
   button?: ButtonProps;
   input?: InputProps;
   label?: JSX.IntrinsicElements["label"];
-  onSubmit?: (values: object) => void;
+  onSubmit?: (search: string) => void;
 };
 
 export const Search = ({
@@ -25,7 +25,8 @@ export const Search = ({
 }: SearchProps) => {
   const handleOnSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    onSubmit?.(getFormValues(e.target as HTMLFormElement));
+    const values = getFormValues(e.target as HTMLFormElement);
+    onSubmit?.(values.search as string);
   };
 
   return (
@@ -36,7 +37,7 @@ export const Search = ({
     >
       <Label htmlFor="search" {...label} />
       <div className="container">
-        <Input id="search" name="search" {...input} />
+        <Input id="search" name="search" required={true} {...input} />
         <Button type="submit" {...button}>
           Pesquisar
         </Button>
