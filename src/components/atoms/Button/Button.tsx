8667d3466d32062cb0179de7a04739fc289ff03f
Jonatas Evangelista <jonatasge@gmail.com>
2023-09-22 13:17:37 -0300

    refactor: renames CSS vars

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -3,7 +3,7 @@ export type ButtonProps = JSX.IntrinsicElements["button"];
 export const Button = ({ className, ...props }: ButtonProps) => {
   return (
     <button
-      className={`Button transition-shadow shadow hover:shadow focus:shadow ${
+      className={`Button transition transition-shadow shadow hover:shadow focus:shadow ${
         className || ""
       }`}
       {...props}
--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -3,7 +3,7 @@ export type InputProps = JSX.IntrinsicElements["input"];
 export const Input = ({ className, ...props }: InputProps) => {
   return (
     <input
-      className={`Input transition-shadow shadow hover:shadow focus:shadow ${
+      className={`Input transition transition-shadow shadow hover:shadow focus:shadow ${
         className || ""
       }`}
       {...props}
