60a9774169710c87d825f115fe68f9732ca009a4
Jonatas Evangelista <jonatasge@gmail.com>
2023-09-22 14:33:10 -0300

    feat: adds Link prop "as

--- src/components/atoms/Link/Link.tsx
+++ src/components/atoms/Link/Link.tsx
@@ -1,5 +1,9 @@
-export type LinkProps = JSX.IntrinsicElements["a"];
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export type LinkProps = JSX.IntrinsicElements["a"] & {
+  as?: keyof JSX.IntrinsicElements;
+};
 
-export const Link = ({ className, ...props }: LinkProps) => {
-  return <a className={`Link ${className || ''}`} {...props} />;
+export const Link = ({ as = "a", className, ...props }: LinkProps) => {
+  const As = as as any;
+  return <As className={`Link ${className || ""}`} {...props} />;
 };
