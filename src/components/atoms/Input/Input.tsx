e0056640bf5701d2d9891d6eaba216c30d985a6c
Jonatas Evangelista <jonatasge@gmail.com>
2023-09-25 16:13:54 -0300

    feat: Input focus on click

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useRef } from "react";
 
 export type InputProps = JSX.IntrinsicElements["input"] & {
   before?: ReactNode;
@@ -6,10 +6,15 @@ export type InputProps = JSX.IntrinsicElements["input"] & {
 };
 
 export const Input = ({ after, before, className, ...props }: InputProps) => {
+  const ref = useRef<HTMLInputElement>(null);
+
   return (
-    <div className={`Input transition transition-bg-color ${className || ""}`}>
+    <div
+      className={`Input transition transition-bg-color ${className || ""}`}
+      onClick={() => ref.current?.focus()}
+    >
       {before}
-      <input {...props} />
+      <input ref={ref} {...props} />
       {after}
     </div>
   );
