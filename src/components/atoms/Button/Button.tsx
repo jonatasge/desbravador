da2d5a2e84095c6b459db6b7dadc0745bb173413
Jonatas Evangelista <jonatasge@gmail.com>
2023-09-25 12:21:02 -0300

    feat: improves Button styles

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -3,9 +3,7 @@ export type ButtonProps = JSX.IntrinsicElements["button"];
 export const Button = ({ className, ...props }: ButtonProps) => {
   return (
     <button
-      className={`Button transition transition-shadow shadow hover:shadow focus:shadow ${
-        className || ""
-      }`}
+      className={`Button transition transition-bg-color ${className || ""}`}
       {...props}
     />
   );
